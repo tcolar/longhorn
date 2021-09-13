7dd46f2b7be01398d43e3742b3a93033b54c66e4
Thibaut Colar <tcolar@colar.net>
2021-09-13 15:32:37 +0000

    move shift key

--- qmk_firmware/keyboards/handwired/longhorn/keymaps/default/keymap.c
+++ qmk_firmware/keyboards/handwired/longhorn/keymaps/default/keymap.c
@@ -144,16 +144,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // |--------|---------|---------|---------|--------------------|             |--------|---------|---------|---------|---------|---------| 
     // |  Tab   |    T    |    Q    |    x    |    E     |   R     |             |   Y    |    U    |   UP    |    /    |  PG_UP  |  VOL+   | 
     // |--------|---------|---------|---------|--------------------|             |--------|---------|---------|---------|---------|---------| 
-    // |  Caps  |  Shift  |    A    |    W    |    D     |   F     |             |   G    |   LEFT  |   Dn    |   Right |  PG_DN  |  VOL-   | 
+    // |  Caps  |  I      |    A    |    W    |    D     |   F     |             |   G    |   LEFT  |   Dn    |   Right |  PG_DN  |  VOL-   | 
     // |--------|---------|---------|---------|--------------------|             |--------|---------|---------|---------|---------|---------| 
     // |   J    |     H   |    Z    |    S    |    C     |   V     |             |   B    |   N     |   M     |    +    |   -     |   =     | 
     // ---------|---------|---------|---------|--------------------|             |--------|-------- |---------|---------|---------|---------|
-    //          |  Ctrl   |  Alt    |   Space |    Enter |   I     |             |  K     |  WIN    | BackSpc |  DEL    |  L      |
+    //          |  Ctrl   | Shift   |   Space |    Enter |  Alt    |             |  K     |  WIN    | BackSpc |  DEL    |  L      |
     //          ----------------------------------------------------             --------------------------------------------------
     [_QWERTY] = LAYOUT(
         KC_ESC  , KC_1    , KC_2    , KC_3    , KC_4    , KC_5           ,          KC_6  , KC_7    , KC_8    , KC_9    , KC_0    , TOG_3 , 
         KC_TAB  , KC_T    , KC_Q    , KC_X    , KC_E    , KC_R           ,         KC_Y   , KC_U    , KC_UP   , KC_PSLS , KC_PGUP , KC_VOLU, 
-        KC_CAPS , KC_LSFT , KC_A    , KC_W    , KC_D    , KC_F           ,          KC_G  , KC_LEFT , KC_DOWN , KC_RGHT , KC_PGDN , KC_VOLD, 
+        KC_CAPS , KC_I    , KC_A    , KC_W    , KC_D    , KC_F           ,          KC_G  , KC_LEFT , KC_DOWN , KC_RGHT , KC_PGDN , KC_VOLD, 
         KC_J    , KC_H    , KC_Z    , KC_S    , KC_C    , KC_V           ,          KC_B  , KC_N    , KC_M    , KC_PLUS , KC_MINS , KC_EQL, 
-                  KC_LCTL , KC_LALT , KC_SPC  , KC_ENTER, KC_I           ,        KC_K    , KC_LGUI , KC_BSPC , KC_DEL  , KC_L )
+                  KC_LCTL , KC_LSFT , KC_SPC  , KC_ENTER, KC_LALT        ,        KC_K    , KC_LGUI , KC_BSPC , KC_DEL  , KC_L )
 };
